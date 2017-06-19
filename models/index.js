5dca7f1f3c5f2fe90880e4b2b21aaa2575a63248
thanseehabdulla <thanseehabdulla@gmail.com>
2017-06-19 11:59:06 +0530

    Oauth2 added

--- models/index.js
+++
@@ -1,9 +0,0 @@
-
-var mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/gasterminal', {});
-
-module.exports.oauth = require('./oauth');
-module.exports.User = require('./user');
-module.exports.OAuthClientsModel = require('./oauth_client');
-module.exports.mongoose = mongoose;
--- server.js
+++ server.js
@@ -4,29 +4,24 @@ var bodyParser = require('body-parser');
 
 var path = require('path');
 
-var index = require('./routes/index');
-var tasks = require('./routes/task');
-var users = require('./routes/users');
-var sessions = require('./routes/session');
 
 // Oauth2
-var models = require('./models');
-var middleware = require('./middleware');
-var oauthserver = require('node-oauth2-server');
-var User = models.User;
+var http = require('http');
+
+var passport = require('passport');
+
+var util = require('util');
+var expressValidator = require('express-validator')
+var auth = require("./auth");
+var oauth = require("./oauth");
+var registration = require("./registration")
+
+var index = require('./routes/index');
+var tasks = require('./routes/task');
 
 var app = express();
 
-
 var port = 5000;
-
-
-// oauth
-app.oauth = oauthserver({
-  model: models.oauth,
-  grants: ['password', 'authorization_code', 'refresh_token'],
-  debug: true
-});
 
 //view Engine
 
@@ -40,6 +35,7 @@ app.use(express.static(path.join(__dirname,'client')));
 // body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+app.use(expressValidator());
 
 app.use('/api',tasks);
 
@@ -48,45 +44,16 @@ app.use('/',index);
 
 
 
-// Oauth2
-app.all('/oauth/token', app.oauth.grant());
-app.get('/oauth/authorise', function(req, res, next) {
-  if (!req.session.userId) {
-    return res.redirect('/session?redirect=' + req.path + '&client_id=' +
-      req.query.client_id + '&redirect_uri=' + req.query.redirect_uri);
-  }
 
-  res.render('authorise', {
-    client_id: req.query.client_id,
-    redirect_uri: req.query.redirect_uri
-  });
-});
+app.use(passport.initialize())
 
-// Handle authorise
-app.post('/oauth/authorise', function(req, res, next) {
-  if (!req.session.userId) {
-    return res.redirect('/session?redirect=' + req.path + 'client_id=' +
-      req.query.client_id +'&redirect_uri=' + req.query.redirect_uri);
-  }
+app.post('/oauth/token', oauth.token)
 
-  next();
-}, app.oauth.authCodeGrant(function(req, next) {
-  // The first param should to indicate an error
-  // The second param should a bool to indicate if the user did authorise the app
-  // The third param should for the user/uid (only used for passing to saveAuthCode)
-  next(null, req.body.allow === 'yes', req.session.userId, null);
-}));
+app.post('/users', registration.registerUser)
 
-app.get('/secret', middleware.requiresUser, function(req, res) {
-  res.send('Secret area');
-});
-
-app.use(app.oauth.errorHandler());
-
-app.post('/v1/users', users.create);
-app.get('/account', middleware.requiresUser, users.show);
-app.post('/session', sessions.create);
-app.get('/session', sessions.show);
+app.get('/restricted', passport.authenticate('accessToken', { session: false }), function (req, res) {
+    res.send("Yay, you successfully accessed the restricted resource!")
+})
 
 if(app.path != '/api' && app.path != '/secret')
 {
@@ -99,3 +66,5 @@ if(app.path != '/api' && app.path != '/secret')
 app.listen(port, function(){
     console.log('server started at port:'+port);
 });
+
+
